0b802ce7f3f2193ce538f1bc539edb1f7564bf1b
gsemac <gabe.semac@gmail.com>
2020-07-11 00:54:57 -0600

    Fixed issue with ComoBox text rendering when using static form instances

--- Gsemac.Forms.Styles/Applicators/StyleApplicatorMessageFilter.cs
+++ Gsemac.Forms.Styles/Applicators/StyleApplicatorMessageFilter.cs
@@ -23,6 +23,7 @@ namespace Gsemac.Forms.Styles.Applicators {
 
                 form.Shown += FormShownEventHandler;
                 form.FormClosed += FormClosedEventHandler;
+                form.Disposed += FormDisposedEventHandler;
 
                 seenForms.Add(form);
 
@@ -47,14 +48,39 @@ namespace Gsemac.Forms.Styles.Applicators {
         }
         private void FormClosedEventHandler(object sender, EventArgs e) {
 
+            /* Why clear styles when the form is closed?
+             * I encountered an issue with ComoboBox, where text in the drop-down area would have a different font when re-opening the form (as if UseCompatibleTextRendering was enabled).
+             * This occurred when opening the form through a static instance, where the same instance would be re-opened.
+             * This occurred even if styles were not re-applied, meaning closing the form while styles are applied seems to be triggering the problem.
+             */
+
+            if (sender is Form form) {
+
+                // Hide the form before clearing styles so that the user doesn't see the styles being cleared.
+
+                form.Visible = false;
+
+                styleApplicator.ClearStyles(form);
+
+            }
+
+        }
+        private void FormDisposedEventHandler(object sender, EventArgs e) {
+
             if (sender is Form form && seenForms.Contains(form)) {
 
-                form.Shown -= FormShownEventHandler;
-                form.FormClosed -= FormClosedEventHandler;
+                RemoveEventHandlers(form);
 
                 seenForms.Remove(form);
 
             }
+
+        }
+
+        private void RemoveEventHandlers(Form form) {
+
+            form.Shown -= FormShownEventHandler;
+            form.Disposed -= FormDisposedEventHandler;
 
         }
 
